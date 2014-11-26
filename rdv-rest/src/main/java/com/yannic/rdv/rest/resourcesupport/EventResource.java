87b2d6e6c02e0c4d5517d4198b301cdd92386fd0
Yannic <Yannic@10.0.2.15>
2014-11-25 22:25:52 -0500

    Adding event booking feature

--- rdv-rest/src/main/java/com/yannic/rdv/rest/resourcesupport/EventResource.java
+++ rdv-rest/src/main/java/com/yannic/rdv/rest/resourcesupport/EventResource.java
@@ -3,22 +3,36 @@ package com.yannic.rdv.rest.resourcesupport;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+import java.text.ParseException;
+
 import org.springframework.hateoas.ResourceSupport;
 
 import com.yannic.rdv.data.model.Event;
+import com.yannic.rdv.data.model.Person;
 import com.yannic.rdv.rest.controller.EventCtrl;
 
 public class EventResource extends ResourceSupport {
 	
+	public static final String REL_EVENT_BOOKING = "rdv:bookEvent";
+	
 	private final Event event;
 	
-	public EventResource(Event event) {
+	public EventResource(Event event, Person person) {
 		this.event = event;
 		this.add(linkTo(methodOn(EventCtrl.class).getEvent(event.getEventId())).withSelfRel());
 	}
 
 	public Event getEvent() {
 		return event;
+	}
+	
+	public void addBookingCode(String code) {
+		try {
+			this.add(linkTo(methodOn(EventCtrl.class).bookEvent(event.getEventId(), code)).withRel(REL_EVENT_BOOKING));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
