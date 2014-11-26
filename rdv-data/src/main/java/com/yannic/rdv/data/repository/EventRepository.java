a3bb361b6649249237447a2a8d0419ad0c9c3e4c
Yannic <Yannic@10.0.2.15>
2014-11-25 22:20:31 -0500

    Adding query to fetch available events

--- rdv-data/src/main/java/com/yannic/rdv/data/repository/EventRepository.java
+++ rdv-data/src/main/java/com/yannic/rdv/data/repository/EventRepository.java
@@ -1,16 +1,20 @@
 package com.yannic.rdv.data.repository;
 
+import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.yannic.rdv.data.model.Event;
 import com.yannic.rdv.data.model.Organizer;
+import com.yannic.rdv.data.model.type.EventStatus;
 
 @RepositoryRestResource(exported = false)
 public interface EventRepository extends JpaRepository<Event, Long>{
 
 	List<Event> findByOrganizer(Organizer organizer);
 	
+	List<Event> findByOrganizerAndStatusAndStartDateGreaterThan(Organizer organizer, EventStatus status, Date startDate, Pageable pageable);
 }
