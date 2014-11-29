8c29cb4a8077fd1a800ea541be2e5fbb75cca2c3
Yannic <Yannic@10.0.2.15>
2014-11-28 22:28:22 -0500

    Adding comments

--- rdv-data/src/main/java/com/yannic/rdv/data/model/type/EventStatus.java
+++ rdv-data/src/main/java/com/yannic/rdv/data/model/type/EventStatus.java
@@ -1,7 +1,8 @@
 package com.yannic.rdv.data.model.type;
 
 public enum EventStatus {
-	NEW,
-	AVAILABLE,
-	FULL;
+	NEW, // organizer has just created the event. The event is not yet available for booking
+	AVAILABLE, // the event is available and may be booked
+	FULL, // the event has been booked and may be cancelled by attendee
+	FROZEN // organizer has frozen the event and no modification may be made by the attendee
 }
